606b04f62c133ecdf638c25c5799d7becca3f097
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-07-07 19:05:08 +0300

    Update store.ts with the new separated files (reducers & actions)

--- store/redux/store.ts
+++ store/redux/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
-import todos from "./todos";
+import todoReducers from "./reducers.ts";
 
 const store = configureStore({
     reducer: {
-        todos: todos,
+        todos: todoReducers,
     }
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
