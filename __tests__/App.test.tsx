7347337a39715d2749f507c0e4436e8c442fb258
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-07-09 14:32:17 +0300

    Write the first unit testing ever

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -8,10 +8,20 @@ import App from '../App';
 
 // Note: import explicitly to use the types shipped with jest.
 import {it} from '@jest/globals';
-
+import {Provider} from 'react-redux';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import store from '../store/redux/store';
+
+const ProviderApp = () => {
+    return (
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    );
+};
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+    renderer.create(<ProviderApp/>);
 });
+
