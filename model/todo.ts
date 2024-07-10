81723b19ecf8e3a37f88b9daa398da0b8e9a0d8c
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-07-10 04:46:51 +0300

    Change the Todo class to match Realm requirements

--- model/todo.ts
+++ model/todo.ts
@@ -1,6 +1,17 @@
-type Todo = {
-    id: string;
-    title: string;
+import Realm from 'realm';
+
+class Todo extends Realm.Object {
+    _id!: number;
+    title?: string;
+
+    static schema: Realm.ObjectSchema = {
+        name: 'Todo',
+        primaryKey: '_id',
+        properties: {
+            _id: 'int',
+            title: 'string',
+        },
+    };
 }
 
 export default Todo;
