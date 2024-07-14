f44227afc00f38072fbda650b4aa0ec81fbd349a
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-07-14 16:43:10 +0300

    Update jest configurations for the unit testing

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'react-native',
     transformIgnorePatterns: [
-        'node_modules/(?!(jest-)?@react-native|react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+        'node_modules/(?!(jest-)?@react-native|react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*|@realm|realm)',
     ],
     setupFiles: ['<rootDir>/setup-jest.js'],
 };
