d4ac23d47fa94fcf9523c86229c90b13ddde9011
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2024-07-14 16:42:30 +0300

    Update reducers and actions with the new _id of Todo, adding setTodos case

--- store/redux/actions.ts
+++ store/redux/actions.ts
@@ -1,6 +1,7 @@
 import {createAction} from "@reduxjs/toolkit";
-import Todo from "../../model/todo";
+import {Todo} from "../../model/todo";
 
 export const addTodo = createAction<Todo>("ADD_TODO");
-export const deleteTodo = createAction<string>("DELETE_TODO");
+export const deleteTodo = createAction<number>("DELETE_TODO");
 export const updateTodo = createAction<Todo>("UPDATE_TODO");
+export const setTodos = createAction<Todo[]>("SET_TODOS");
--- store/redux/reducers.ts
+++ store/redux/reducers.ts
@@ -1,6 +1,6 @@
 import {createReducer} from "@reduxjs/toolkit";
-import Todo from "../../model/todo";
-import {addTodo, deleteTodo, updateTodo} from "./actions";
+import {Todo} from "../../model/todo";
+import {addTodo, deleteTodo, setTodos, updateTodo} from "./actions";
 
 interface TodoState {
     todos: Todo[]
@@ -14,20 +14,20 @@ const todoReducers = createReducer(initialState,
     (builder) => {
         builder
             .addCase(addTodo, (state, action) => {
-                console.log("addTodo");
                 state.todos.push(action.payload);
             })
             .addCase(deleteTodo, (state, action) => {
-                console.log("deleteTodo");
-                state.todos = state.todos.filter(todo => todo.id !== action.payload);
+                state.todos = state.todos.filter(todo => todo._id !== action.payload);
             })
             .addCase(updateTodo, (state, action) => {
-                console.log("updateTodo");
-                const index = state.todos.findIndex(todo => todo.id === action.payload.id);
+                const index = state.todos.findIndex(todo => todo._id === action.payload._id);
                 if (index !== -1) {
                     state.todos[index] = action.payload;
                 }
             })
+            .addCase(setTodos, (state, action) => {
+                state.todos = action.payload;
+            });
     });
 
 export default todoReducers;
